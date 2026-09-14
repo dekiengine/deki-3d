2ce5ce9ad98a69be5b303927e870369103fddd20
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-14 22:01:33 +0200

    Docs for deki-3d from deki-editor f449cd3

---
+++ navtreeindex0.js
@@ -0,0 +1,13 @@
+var NAVTREEINDEX0 =
+{
+"index.html":[],
+"index.html#autotoc_md0":[0],
+"index.html#autotoc_md1":[1],
+"index.html#autotoc_md2":[2],
+"index.html#autotoc_md3":[3],
+"index.html#autotoc_md4":[3,0],
+"index.html#autotoc_md5":[3,1],
+"index.html#c-camera3dcomponent":[2,0],
+"index.html#c-meshcomponent":[2,1],
+"pages.html":[]
+};
